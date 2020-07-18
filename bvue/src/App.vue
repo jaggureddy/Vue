2bb26269d47aacf547a7e4f7ee7b490d2441b9f3
JagguReddy <Jagadeesh6010@gmail.com>
2020-07-17 22:24:58 -0400

    vue-2

--- bvue/src/App.vue
+++ bvue/src/App.vue
@@ -1,23 +1,12 @@
 <template>
   <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
-   <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-   <MyTable />
+    <div id="nav">
+      <router-link to="/form">Form</router-link> |
+      <router-link to="/table">Table</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
-
-<script>
-//import HelloWorld from './components/HelloWorld.vue'
-import MyTable from './components/MyTable.vue'
-
-export default {
-  name: 'App',
-  components: {
-    //HelloWorld
-    MyTable
-  }
-}
-</script>
 
 <style>
 #app {
@@ -26,6 +15,15 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+#nav {
+  padding: 30px;
+}
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+#nav a.router-link-exact-active {
+  color: #42b983;
 }
 </style>
